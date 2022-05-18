9352bb49159831a83f779d4dcb2ce96c34da1246
Tanaka Lusengo <79418686+tanaka-lusengo@users.noreply.github.com>
2022-05-18 20:42:21 +0100

    updated https request

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const CHARACTER_API_URL = "http://gateway.marvel.com/v1/public/characters?";
+const CHARACTER_API_URL = "https://gateway.marvel.com/v1/public/characters?";
 
 const tsHash1 =
   "ts=1&apikey=d11e48a9c2a3ac9be1f3457a5899c1ac&hash=2b18905bed04764370c0b5b409f210a8";
